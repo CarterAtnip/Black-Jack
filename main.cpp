15fa6f9004cf5bf98f302724b82ec8689e06eb4f
Carter <carter.atnip@gmail.com>
2024-09-11 23:02:24 -0500

    Created CMake file and made sure project ran

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// main cpp file for blackjack project
+// Created by carte on 9/11/2024.
+//
+
+#include "main.h"
+
+int main(){
+cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+// Main function header for black jack project
+// Created by Carter on 9/11/2024.
+//
+
+#ifndef BLACKJACK_MAIN_H
+#define BLACKJACK_MAIN_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#endif //BLACKJACK_MAIN_H
