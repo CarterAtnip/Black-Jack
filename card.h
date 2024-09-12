3a638faac5aaa3469b0fe0275a13eb5740655d23
Carter <carter.atnip@gmail.com>
2024-09-11 23:13:43 -0500

    Created card struct that stores suite and value

---
+++ card.h
@@ -0,0 +1,17 @@
+// card struct
+// Created by carte on 9/11/2024.
+//
+
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+
+using std::string;
+
+struct Card
+{
+    string suite;
+    string value;
+};
+#endif //CARD_H
